cbed05b64b00444710162712172ab0c460a630fb
Pedro Vitorio <phjvitorio@gmail.com>
2022-05-31 18:42:21 -0300

    Implementing active search field

--- src/App.js
+++ src/App.js
@@ -3,15 +3,34 @@ import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import {robots} from './robots';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots}/>
-        </div>
-        
-    )
+class App extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+            
+        )
+    }
+    
 }
 
 export default App;
